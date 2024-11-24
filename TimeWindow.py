d490b9eb40c8dbf48f8c89f139a52b8d962323bd
laitinenp <petri.laitinen@karelia.fi>
2024-11-24 10:37:22 +0200

    started developing TimeWindow class

--- TimeWindow.py
+++ TimeWindow.py
@@ -1,11 +1,20 @@
-
+import json
 
 class TimeWindow:
 
     def __init__(self, rows, cols):
-        data = [[0]*cols]*rows
+        self.data = [[0]*cols]*rows
+        self.index = 0
+        self.config = json.load(open('config.json'))
 
     def get(self, row, col):
-        return data[row][col]
+        return self.data[row][col]
     
+    # row is relational, starting from index
+    def put(self, row, col, value):
+        self.data[(self.index+row)%self.config.rows][col] = value
 
+    def move():
+        self.index = self.index + 1
+        if self.index == self.config.rows :
+            self.index = 0
